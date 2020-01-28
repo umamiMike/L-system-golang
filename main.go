8b1ecc4729b5939a7b08df14f0ae6f0d1f1f6488
Mike Wilding <michaelwchastain@gmail.com>
2020-01-27 18:54:58 -0800

    removes duplicate struct

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import "fmt"
 
-type System struct {
-	axiom      string //the start, must be one of the keys in the rules
-	iterations int    //the number of times to recurse through the set
-	outfile    string
-	rules      string
-}
-
 func main() {
 
 	sys := System{
@@ -17,5 +10,5 @@ func main() {
 		axiom:      "a",
 	}
 	grammer := sys.generate()
-	fmt.Print(grammer[3])
+	fmt.Print(grammer)
 }
